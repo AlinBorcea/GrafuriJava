6594868f445138fc135d54e6605d55f614221023
AlinBorcea <alinborcea2@gmail.com>
2019-07-12 16:43:34 +0300

    theorems and sentences can be easily added in strings.xml

--- app/src/main/java/com/alin/grafuri/Lesson.java
+++ app/src/main/java/com/alin/grafuri/Lesson.java
@@ -5,13 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Objects;
+
 public class Lesson extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
-        TextView textView = (TextView) findViewById(R.id.text_view_lesson);
-        textView.setText(String.valueOf(getIntent().getIntExtra(Utils.lessonExtra, -1)));
+        TextView lessonContent = findViewById(R.id.text_view_lesson);
+        lessonContent.setText(getLesson(
+                Objects.requireNonNull(getIntent().getStringExtra(Utils.chapterExtra)),
+                getIntent().getIntExtra(Utils.lessonExtra, -1)));
+
     }
+
+    private String getLesson(String chapter, int position) {
+
+        switch (chapter) {
+
+            case Utils.theorem:
+                return getResources().getStringArray(R.array.theorem_lessons)[position];
+
+            case Utils.sentence:
+                return getResources().getStringArray(R.array.sentence_lessons)[position];
+
+                default: return "No lessons!";
+
+        }
+
+    }
+
 }
